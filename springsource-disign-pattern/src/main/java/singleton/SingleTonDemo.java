869b03a9a93e7dd106a2c01ba8df282daeec9e3a
wangkun <249900859@qq.com>
2019-07-13 23:56:22 +0800

    单例设计模式

---
+++ springsource-disign-pattern/src/main/java/singleton/SingleTonDemo.java
@@ -0,0 +1,37 @@
+package singleton;
+
+import sun.security.jca.GetInstance;
+
+/**
+ * @author wangkun
+ * @date 2019-07-13 23:37
+ */
+public class SingleTonDemo {
+
+    //static 保证全局唯一
+    //private static SingleTonDemo instance = null ;
+    //改进为静态内部类,不用静态方法块的原因,保证在调用的时候再声明不出来，不浪费空间
+    private static class LazyHoler{
+        private  static final SingleTonDemo  instance = new SingleTonDemo();
+    }
+
+    //静态方法保证实例不被外部new出来
+    private SingleTonDemo() {
+    }
+
+    //final 防止被覆盖
+    public final  static SingleTonDemo getInstance() {
+/*        if(instance == null ){
+            instance = new SingleTonDemo();
+        }*/
+        return LazyHoler.instance ;
+    }
+
+    public static void main(String[] args) {
+
+        SingleTonDemo s1 = SingleTonDemo.getInstance() ;
+        SingleTonDemo s2 = SingleTonDemo.getInstance() ;
+        System.out.println(s1.equals(s2));
+
+    }
+}
